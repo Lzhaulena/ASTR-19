369bf74d1e5ce2cba66fcc44655bf3630c976607
Lzhaulena <lukas07haulena@gmail.com>
2026-04-08 17:46:31 -0700

    Add files via upload

---
+++ LukasPrint.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:19:24 2026
+
+@author: Lukas
+"""
+
+def PrintLukas():
+    print("Lukas Haulena, He/him, I can't say I have a favorite movie but I really enjoy the movie Nope, and my favorite food is sushi.")
+    
+def main():
+    PrintLukas()
+    
+if __name__ == "__main__":
+    main()
+    

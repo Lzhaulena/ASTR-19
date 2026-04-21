a04fad2c3354ce6034820a16d3940cac11b7be72
Lzhaulena <lukas07haulena@gmail.com>
2026-04-21 07:58:02 -0700

    Add files via upload

---
+++ Journal_prompt2.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 15 17:55:25 2026
+
+@author: Lukas
+"""
+
+float1 = 13.75
+float2 = 6.5
+sum_result = float1 + float2
+
+
+print(f"sum: {sum_result}")
+print(f"Data Type of Sum: {type(sum_result)}")
+print("-" * 30)
+
+
+int1 = 45
+int2 = 12
+diff_result = int1 - int2
+
+print(f"Difference: {diff_result}")
+print(f"Data Type of Difference: {type(diff_result)}")
+print("-" * 30)
+
+
+
